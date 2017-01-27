2f9355ee640538823e3e654f4bb8398b27cd8cda
Piers Mainwaring <piers@impossibly.org>
2017-01-27 12:09:01 -0700

    Adds tests for endpointAction

---
+++ test/src/crud/action.test.js
@@ -0,0 +1,43 @@
+/* eslint-env jest */
+
+import endpointAction from '~/src/crud/action'
+import {
+  expectDispatchedAction,
+  mockDispatch
+} from '~/test/__helpers__/redux'
+
+
+const endpointName = 'index'
+const getState = () => ({
+  [endpointName]: {
+    isFetching: false,
+    timestamp: Date.now(),
+    data: null,
+    error: null
+  }
+})
+const dispatch = mockDispatch((action) => typeof action === 'function' ? action(dispatch, getState) : action)
+
+beforeEach(() => dispatch.mockClear())
+
+describe('action', () => {
+  describe('on successful response', () => {
+    it('dispatches the right lifecycle actions', () => {
+      const requestAction = endpointAction(endpointName, () => Promise.resolve())
+      return dispatch(requestAction()).then(() => {
+        expectDispatchedAction(dispatch, { type: 'index.request', isFetching: true })
+        expectDispatchedAction(dispatch, { type: 'index.success', isFetching: false })
+      })
+    })
+  })
+
+  describe('on failed response', () => {
+    it('dispatches the right lifecycle actions', () => {
+      const requestAction = endpointAction(endpointName, () => Promise.reject())
+      return dispatch(requestAction()).then(() => {
+        expectDispatchedAction(dispatch, { type: 'index.request', isFetching: true })
+        expectDispatchedAction(dispatch, { type: 'index.failure', isFetching: false })
+      })
+    })
+  })
+})
